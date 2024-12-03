4db00610eabd484003ad81ac1319f8bb8b754908
Doye <edyogoba23@gmail.com>
2024-12-03 14:47:18 +0100

    added telegram script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
       >
         {children}
+        <Script src="https://telegram.org/js/telegram-web-app.js?56" strategy="beforeInteractive"/>
         <NavBar />
       </body>
     </html>
