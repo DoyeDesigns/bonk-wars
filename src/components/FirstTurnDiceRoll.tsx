393ba633721e23081989612e34e6dcf42ba6d929
Doye <edyogoba23@gmail.com>
2024-12-09 11:06:20 +0100

    corrected build error

--- src/components/FirstTurnDiceRoll.tsx
+++ src/components/FirstTurnDiceRoll.tsx
@@ -6,7 +6,6 @@ import React, { useEffect, useState } from "react";
 const DiceRollToDetermineFirstTurn = () => {
     const { checkDiceRollsAndSetTurn, rollAndRecordDice, gameState } = useOnlineGameStore();
 
-    const [rollNumber, setRollNumber] = useState<number | null>(null);
     const [telegramUserId, setTelegramUserId] = useState<number | null>(null);
 
     useEffect(() => {
